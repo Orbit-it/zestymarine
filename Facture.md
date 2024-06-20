0bf031048461e7afa6b542424d593193e954d597
Orbit-it <mansourdiop0011@gmail.com>
2024-06-20 23:30:22 +0100

    first commit

---
+++ Facture.md
@@ -0,0 +1,45 @@
+```markdown
+Orbit-iT
+Dakar, Sénégal
+
+**Facture N° :** 2024-001
+
+**Date :** 31 mai 2024
+
+**À :**  
+Zesti-marine   
+Bizerte, Tunisie
+
+Détail de la Facture
+
+| Description                       | Quantité | Prix Unitaire (TND) | Montant (TND) |
+|-----------------------------------|----------|---------------------|---------------|
+| Création du site web Zesti-marine | 1        | 1500                | 1500          |
+
+---
+
+**Montant Total à Payer :** 1500 TND
+
+
+**Coordonnées Bancaires :**
+- **Banque :** BIAT
+- **Nom du compte :** Serigne Mansour Diop
+- **Numéro de compte :** 123456789
+- **Code IBAN :** TN59123456789123456789
+- **Code BIC/SWIFT :** BTNITNTT
+
+---
+
+Nous vous remercions de votre confiance et restons à votre disposition pour toute question ou information complémentaire.
+
+Cordialement,
+
+**Orbit-iT**  
+Contact : +216 53 866 594 
+Email : contact@orbit-it.io  
+Site web : [www.orbit-it.io](http://www.orbit-it.io)
+
+---
+
+**Remarque :** Cette facture concerne uniquement la première phase de la création du site web Zesti-marine. D'autres phases et leurs coûts respectifs seront facturés ultérieurement selon l'avancement du projet.
+```
---
+++ core/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+from .views import (
+    ItemDetailView,
+    HomeView,
+    add_to_cart,
+    remove_from_cart,
+    demande_devis,
+    ShopView,
+    OrderSummaryView,
+    remove_single_item_from_cart,
+    CheckoutView,
+    PaymentView,
+    AddCouponView,
+    RequestRefundView,
+    CategoryView
+)
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('demande_devis', demande_devis, name='demande_devis'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('category/<slug>/', CategoryView.as_view(), name='category'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add_coupon/', AddCouponView.as_view(), name='add-coupon'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('services/', ShopView.as_view(), name='shop'),
+    path('boutique/', OrderSummaryView.as_view(), name='order-summary'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+         name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/', RequestRefundView.as_view(), name='request-refund')
+]
