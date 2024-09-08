f97f110a8b09c332e086964e2243f2b2654c0378
orbit-it <mansourdiop0011@gmail.com>
2024-09-08 15:58:17 +0000

    version2

--- demo/settings.py
+++ demo/settings.py
@@ -5,7 +5,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-49-66-47.eu-north-1.compute.amazonaws.com', 'https://ec2-13-49-66-47.eu-north-1.compute.amazonaws.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
